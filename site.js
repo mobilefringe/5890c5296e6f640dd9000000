2a2f87666faf1f9aad084a38114b44177a6581f9
ardalan@mobilefringe.com @ CodeCloud <ardalan@mobilefringe.com @ CodeCloud>
2017-01-31 12:54:20 -0500

    Twin Pine Demo 1.2  /site.js

---
+++ site.js
@@ -0,0 +1,8 @@
+var site_json = {
+                    "name" : "The Shops at Rossmoor",
+                    "default_image" : "//codecloud.cdn.speedyrails.net/sites/58332abd6e6f646f31040000/image/jpeg/1484182955000/dph_500x500_logo.jpg",
+                    "time_zone" : "T08:00:00Z",
+                    "social_feed" : "//longbeach.mallmaverick.com/api/v2/longbeach/social.json"
+                }
+                
+               
