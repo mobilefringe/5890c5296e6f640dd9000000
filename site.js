80e84109516f63517f67ec678f19f1c9a600442b
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-05-25 10:24:00 -0400

    Twin Pine Demo 1.2  /site.js

--- site.js
+++ site.js
@@ -3,7 +3,7 @@ var site_json = {
                     "default_image" : "//codecloud.cdn.speedyrails.net/sites/5890c5296e6f640dd9000000/image/jpeg/1485886146000/twinlogo.jpg",
                     "time_zone" : "T08:00:00Z",
                     "social_feed" : "//longbeach.mallmaverick.com/api/v2/longbeach/social.json"
-                }
+                };
                 
 /*! Magnific Popup - v0.9.9 - 2013-11-15
 * http://dimsemenov.com/plugins/magnific-popup/
@@ -1947,5 +1947,7 @@ $.magnificPopup.registerModule(RETINA_NS, {
 
 		}
 	}
+});
+
 });
                
