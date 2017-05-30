44536cee00d92f4156760cd3e0b80af25c9068e4
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-05-30 14:17:25 -0400

    Twin Pine Demo 1.2  /site.js

--- site.js
+++ site.js
@@ -38,7 +38,7 @@ if ('serviceWorker' in navigator && 'PushManager' in window) {
     console.log('Service Worker is registered', swReg);
 
     swRegistration = swReg;
-    initialiseUI();
+   
   })
   .catch(function(error) {
     console.error('Service Worker Error', error);
