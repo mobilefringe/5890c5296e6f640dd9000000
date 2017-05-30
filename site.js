9bd433121ca23b69ea32075b64b470f56ff4ace8
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-05-30 12:08:15 -0400

    Twin Pine Demo 1.2  /site.js

--- site.js
+++ site.js
@@ -6,6 +6,7 @@ var site_json = {
                 };
 const applicationServerPublicKey = 'BPJ-LpAmMhqMLLIfpTwCHhJltp3h6uQu6FuIPF0CX1q7qGGPLlEX3zMYEnsJJF0j8cGnI_z6-gZqt1nFmSDRnHk';
 
+function initialiseUI() { }
 
 const pushButton = document.querySelector('#allowPush');
 console.log(pushButton);
