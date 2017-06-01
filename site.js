098f0377a4986f0cd3dda9e48a5d6ce71fbae2e7
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-06-01 11:55:06 -0400

    Twin Pine Demo 1.2  /site.js

--- site.js
+++ site.js
@@ -86,8 +86,8 @@ function updateBtn() {
     document.querySelector('.popup_header').textContent = "Oh NO!";
     subscriptionJson.textContent = "You have blocked notifications from us. Please enable it from settings and try again!";
     //const popup_home =  document.querySelector('.popup_home');
-    const custom_backdrop = document.querySelector('.receiveNotification');
-    popup_home.parentNode.removeChild(popup_home);
+    const receiveN = document.querySelector('.receiveNotification');
+    receiveN.parentNode.removeChild(receiveN);
     //custom_backdrop.parentNode.removeChild(custom_backdrop);
     pushButton.textContent = 'Push Messaging Blocked.';
     pushButton.disabled = true;
