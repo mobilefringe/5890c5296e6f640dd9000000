8be18699228bcfce07fac6267a8e6b5ee7d83e94
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-06-01 11:01:47 -0400

    Twin Pine Demo 1.2  /site.js

--- site.js
+++ site.js
@@ -123,10 +123,12 @@ function updateSubscriptionOnServer(subscription) {
     document.querySelector('.popup_content');
 
   if (subscription) {
-    document.querySelector('.popup_header').textContent = "THANK YOU!";
-    subscriptionJson.textContent = "Thank you for enrolling to receive notification from us!";
+    
     console.log(JSON.stringify(subscription));
     postToServer(JSON.stringify(subscription));
+    
+    document.querySelector('.popup_header').textContent = "THANK YOU!";
+    subscriptionJson.textContent = "Thank you for enrolling to receive notification from us!";
     subscriptionDetails.classList.remove('is-invisible');
   } else {
     subscriptionDetails.classList.add('is-invisible');
