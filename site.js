9dc8cf4c04d5e835a58c5b073874046bc1f08c38
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-06-20 10:15:08 -0400

    Twin Pine Demo 1.2  /site.js

--- site.js
+++ site.js
@@ -4,7 +4,8 @@ var site_json = {
                     "time_zone" : "T08:00:00Z",
                     "social_feed" : "//longbeach.mallmaverick.com/api/v2/longbeach/social.json"
                 };
-const applicationServerPublicKey = 'BCgQcwuTythf5q2fp8MW1A-JKjTr2oHNSbwdjB-4hG_9kn1EsH7o76m5IIHu4wTmqKHBzSKcZfM0s-hlk29aFOc';//BI830BNKcZ77GIvZTxyefoVzP9Jn865CLKIymavoAc-3_Uo4mSfrrtpZDcyOFXz0Ttyr40IHPUrVF54qXv-920Q=';
+const applicationServerPublicKey = 'BCgQcwuTythf5q2fp8MW1A-JKjTr2oHNSbwdjB-4hG_9kn1EsH7o76m5IIHu4wTmqKHBzSKcZfM0s-hlk29aFOc';
+//BI830BNKcZ77GIvZTxyefoVzP9Jn865CLKIymavoAc-3_Uo4mSfrrtpZDcyOFXz0Ttyr40IHPUrVF54qXv-920Q=';
 var pushButton;
 function activatePushButton() {
     pushButton = document.querySelector('#allowPush');
