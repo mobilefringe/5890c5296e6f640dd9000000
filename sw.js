18bd67c113417a41cfb0fc0896ffdc40f2ddd5e4
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-06-21 12:57:25 -0400

    Twin Pine Demo 1.2  /sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 
 self.addEventListener('push', function(event) {
   console.log('[Service Worker] Push Received.');
-  console.log('[Service Worker] Push had this data:' , ${event.data.text()});
+  console.log('[Service Worker] Push had this data: "${event.data.text()}"');
     console.log(event);
   const title = 'Mall Maverick';
   const options = {
