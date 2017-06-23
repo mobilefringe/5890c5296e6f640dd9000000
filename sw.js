748b133e83cf60a5503d689d1b2e12616791b60b
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-06-23 14:18:00 -0400

    Twin Pine Demo 1.2  /sw.js

--- sw.js
+++ sw.js
@@ -10,8 +10,8 @@ self.addEventListener('push', function(event) {
   const title = (event.data && event.data.text()) || 'Mall Maverick';
   const options = {
     body: 'Mall Maverick Rocks!',
-    // icon: '//codecloud.cdn.speedyrails.net/sites/5890c5296e6f640dd9000000/image/png/1496327260000/823841_message_512x512.png',
-    // badge: 'images/badge.png'
+    icon: '//codecloud.cdn.speedyrails.net/sites/5890c5296e6f640dd9000000/image/png/1496327260000/823841_message_512x512.png',
+    badge: 'images/badge.png'
   };
 
   event.waitUntil(self.registration.showNotification(title, options));
