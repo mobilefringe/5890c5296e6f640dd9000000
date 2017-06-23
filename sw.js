79344203eff5d069159811da7c94392916df6ea1
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-06-23 15:27:41 -0400

    Twin Pine Demo 1.2  /sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,40 @@
 'use strict';
 
 self.addEventListener('push', function(event) {
-  console.log('[Service Worker] Push Received.');
-  console.log('[Service Worker] Push had this data: "${event.data.text()}"');
-  if (event.data) {
-    console.log(event.data.json());
-  }
+//   console.log('[Service Worker] Push Received.');
+//   console.log('[Service Worker] Push had this data: "${event.data.text()}"');
+//   if (event.data) {
+//     console.log(event.data.json());
+//   }
   
-  const title = (event.data && event.data.text()) || 'Mall Maverick';
-  const options = {
-    body: 'Mall Maverick Rocks!',
-    icon: '//codecloud.cdn.speedyrails.net/sites/5890c5296e6f640dd9000000/image/png/1496327260000/823841_message_512x512.png',
-    badge: 'images/badge.png'
-  };
+//   const title = (event.data && event.data.text()) || 'Mall Maverick';
+//   const options = {
+//     body: 'Mall Maverick Rocks!',
+//     icon: '//codecloud.cdn.speedyrails.net/sites/5890c5296e6f640dd9000000/image/png/1496327260000/823841_message_512x512.png',
+//     badge: 'images/badge.png'
+//   };
 
-  event.waitUntil(self.registration.showNotification(title, options));
+//   event.waitUntil(self.registration.showNotification(title, options));
+const analyticsPromise = pushReceivedTracking();
+  const pushInfoPromise = fetch('/api/get-more-data')
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(response) {
+      const title = response.data.userName + ' says...';
+      const message = response.data.message;
+
+      return self.registration.showNotification(title, {
+        body: message
+      });
+    });
+
+  const promiseChain = Promise.all([
+    analyticsPromise,
+    pushInfoPromise
+  ]);
+
+  event.waitUntil(promiseChain);
 });
 
 self.addEventListener('notificationclick', function(event) {
