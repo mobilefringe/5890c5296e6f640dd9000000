79b60dd10ee7af319432feb53444dcb251961dfe
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-06-26 10:39:59 -0400

    Twin Pine Demo 1.2  /site.js

--- site.js
+++ site.js
@@ -92,6 +92,7 @@ function subscriptionExist () {
     const subscriptionDetails = document.querySelector('.popup_content');
     if (isSubscribed) {
         console.log('User IS subscribed.');
+         console.log('User IS subscribed.');
         document.querySelector('.popup_header').textContent = "THANK YOU!";
         subscriptionJson.textContent = "You have already enrolled to receive notification from us!";
         subscriptionDetails.classList.remove('is-invisible');
