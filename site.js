d75188373d07b9f372199c01b705adc09f2eed6a
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-07-04 17:19:43 -0400

    Twin Pine Demo 1.2  /site.js

--- site.js
+++ site.js
@@ -208,7 +208,7 @@ function unsubscribeUser() {
     isSubscribed = false;
 
     updateBtn();
-    $('.popup_json').text("Stay updated with what's new. Get notifications from us about mall news, promotions and more!");
+    $('.popup_header').text("Stay updated with what's new. Get notifications from us about mall news, promotions and more!");
      $('.popup_json').text("Please allow notifications, when prompted!");
   });
 }
