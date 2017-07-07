845c31568415d6081e7d9569e664ebcee062db7d
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-07-07 13:56:32 -0400

    Twin Pine Demo 1.2  /sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 'use strict';
-
+var linkToOpen= "";
 self.addEventListener('push', function(event) {
 //   console.log('[Service Worker] Push Received.');
 //   console.log('[Service Worker] Push had this data: "${event.data.text()}"');
