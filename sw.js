2f76b378c712241f87e0446f216048820b48e7c2
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-07-12 12:33:27 -0400

    Twin Pine Demo 1.2  /sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,7 @@
 'use strict';
 var linkToOpen= "www.google.ca";
 self.addEventListener('push', function(event) {
-//   console.log('[Service Worker] Push Received.');
-//   console.log('[Service Worker] Push had this data: "${event.data.text()}"');
-//   if (event.data) {
-//     console.log(event.data.json());
-//   }
-  
-//   const title = (event.data && event.data.text()) || 'Mall Maverick';
-//   const options = {
-//     body: 'Mall Maverick Rocks!',
-//     icon: '//codecloud.cdn.speedyrails.net/sites/5890c5296e6f640dd9000000/image/png/1496327260000/823841_message_512x512.png',
-//     badge: 'images/badge.png'
-//   };
 
-//   event.waitUntil(self.registration.showNotification(title, options));
-    // const analyticsPromise = pushReceivedTracking();
     const pushInfoPromise = fetch('https://mallmaverickstaging.com/api/v4/twinpine/get_webpush_message')
     .then(function(response) {
         //console.log(response.json());
