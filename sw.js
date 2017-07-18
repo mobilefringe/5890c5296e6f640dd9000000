92dba4c90e208bc27d90daf35ec8e2eccf0168a0
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-07-18 16:39:51 -0400

    Twin Pine Demo 1.2  /sw.js

--- sw.js
+++ sw.js
@@ -58,10 +58,7 @@ self.addEventListener('notificationclick', function(event) {
 self.addEventListener('notificationclose', function(event) {
     var postData= {};
     postData.data = push_message;
-    // $.post("https://mallmaverickstaging.com/api/v4/twinpine/add_webpush_close", postData, function(data, status, xhr){
-    //     console.log(data,status);
-    // });
-    // postAjax('https://mallmaverickstaging.com/api/v4/twinpine/add_webpush_close', postData, function(data){ console.log(data); });
+
     const pushInfoPromise = fetch("https://mallmaverickstaging.com/api/v4/twinpine/add_webpush_close",
     {
         method: "POST",
