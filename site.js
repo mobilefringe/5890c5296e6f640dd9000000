acd2618c757e5f6fbc70e3f2e7f766059facb7eb
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-08-21 14:00:25 -0400

    Twin Pine Demo 1.2  /site.js

--- site.js
+++ site.js
@@ -212,6 +212,17 @@ function unsubscribeUser() {
       postData.data.type1=type1;
       postData.data.type2=type2;
       postData.data.store_id=store_id;
+      $.post("https://mallmaverickstaging.com/api/v4/twinpine/unsubscribe_webpush", postData, function(data, status, xhr){
+        console.log(data,status);
+        if(status == "success"){
+            $('.popup_header').text("Stay updated with what's new. Get notifications from us about mall news, promotions and more!");
+            $('.popup_json').text("Please allow notifications, when prompted!");
+        }
+        else{
+            $('.popup_header').text("SORRY!");
+            $('.popup_json').text("We've ran into an error processing your request. Please try again later!");  
+        }
+        });
       return subscription.unsubscribe();
     }
   })
