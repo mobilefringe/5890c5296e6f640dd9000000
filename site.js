4bd19a74fcc2f5f2628f3cdd64bad06934024bc7
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-08-21 10:53:53 -0400

    Twin Pine Demo 1.2  /site.js

--- site.js
+++ site.js
@@ -34,7 +34,7 @@ function urlB64ToUint8Array(base64String) {
 if ('serviceWorker' in navigator && 'PushManager' in window) {
   console.log('Service Worker and Push is supported');
 
-  navigator.serviceWorker.register('sw.js')
+  navigator.serviceWorker.register('/sw.js')
   .then(function(swReg) {
     console.log('Service Worker is registered', swReg);
 
