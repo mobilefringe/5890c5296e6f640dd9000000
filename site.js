60afe765db3b5558fb7bc2274b77980c01fb1296
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-08-28 12:53:04 -0400

    Twin Pine Demo 1.2  /site.js

--- site.js
+++ site.js
@@ -90,7 +90,7 @@ function initialiseUI() {
       subscribeUser();
     }
     removePermissionModal();
-    updateBlockedStatus
+    updateBlockedStatus();
   });
 }
 function subscriptionExist () {
