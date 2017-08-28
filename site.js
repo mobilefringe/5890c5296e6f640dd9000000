8b7385fbb58dfcd2db28025abe1284361901d40c
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-08-28 11:58:24 -0400

    Twin Pine Demo 1.2  /site.js

--- site.js
+++ site.js
@@ -7,6 +7,10 @@ var site_json = {
 const applicationServerPublicKey = 'BCAfjvLW8NcXbiNzky7G63eyp94KA29XANq7zB30hBd-eIyHGBFCTkPy0rVHEAEvs0H3ltWgIiQs_Kawyfmxcdg=';
 var pushButton;
 var type1, type2, store_id;
+
+subscriptionExist
+
+
 function activatePushButton(typ1,typ2, store) {
     pushButton = $('#allowPush');
     
