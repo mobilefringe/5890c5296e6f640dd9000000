f7ffcb4e89be766046a488ec6110d5898aa33a16
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-08-28 13:24:48 -0400

    Twin Pine Demo 1.2  /site.js

--- site.js
+++ site.js
@@ -133,6 +133,7 @@ function subscriptionExist () {
             $.get('https://mallmaverickstaging.com/api/v4/twinpine/get_promotion_subscriptions', postData, function(data) {
                 console.log("promo");
                 console.log(data);
+                window.subscribed_to_promo = true;
             });
         }
       });
