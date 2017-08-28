95df42b34e097ea80b4a7be4d235a22262f6b734
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-08-28 12:59:44 -0400

    Twin Pine Demo 1.2  /site.js

--- site.js
+++ site.js
@@ -137,7 +137,7 @@ function subscriptionExist () {
                 
             });
             //check what kind of subscription they signed up for
-            const pushInfoPromise3 = fetch('https://mallmaverickstaging.com/api/v4/twinpine/get_promotion_subscriptions?data=' + postData)
+            const pushInfoPromise3 = fetch('https://mallmaverickstaging.com/api/v4/twinpine/get_promotion_subscriptions??data[endpoint]=' + postData.data.endpoint)
             .then(function(response) {
                 console.log(response.json());
                 return response.json();
