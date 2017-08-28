25c7a88dcae8cae2d1e73455e400e8829e1b03f1
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-08-28 13:23:28 -0400

    Twin Pine Demo 1.2  /site.js

--- site.js
+++ site.js
@@ -5,6 +5,7 @@ var site_json = {
                     "social_feed" : "//longbeach.mallmaverick.com/api/v2/longbeach/social.json"
                 };
 window.subscribed_store_ids = [];
+window.subscribed_store_ids = [];
 const applicationServerPublicKey = 'BCAfjvLW8NcXbiNzky7G63eyp94KA29XANq7zB30hBd-eIyHGBFCTkPy0rVHEAEvs0H3ltWgIiQs_Kawyfmxcdg=';
 var pushButton;
 var type1, type2, store_id;
