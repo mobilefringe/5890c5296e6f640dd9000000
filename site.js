0853100e0b66b43b024f5399f8eefafe597dcdbe
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-08-29 11:27:36 -0400

    Twin Pine Demo 1.2  /site.js

--- site.js
+++ site.js
@@ -232,6 +232,7 @@ function unsubscribeUser(typ1,typ2, store) {
                         return value != parseInt(store_id);
                     });
                 }
+                if(unsubscribe_from_browser)
                 return subscription.unsubscribe();
                 
             }
