72dcca7c5bf32d757a8d9635bc771ea316aa33f6
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-08-29 11:39:43 -0400

    Twin Pine Demo 1.2  /site.js

--- site.js
+++ site.js
@@ -289,6 +289,9 @@ function errorSubscribedStatus () {
 
 function defaultSubscribedStatus () {
     console.log("default status");
+    if() {
+        
+    }
     $('.popup_header').text("Stay updated with what's new. Get notifications from us about mall news, promotions and more!");
     $('.popup_json').text("Please allow notifications, when prompted!");
     $('#allowPush').show();
