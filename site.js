53955dc64a1de045b8586a5cef7394f3078993e8
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-08-29 12:09:05 -0400

    Twin Pine Demo 1.2  /site.js

--- site.js
+++ site.js
@@ -214,9 +214,11 @@ function unsubscribeUser(typ1,typ2, store) {
                 
                 //set up different types
                 if(("events").indexOf(type1) > -1 || ("events").indexOf(type2) > -1) {
+                    console.log("unsubscribing from events");
                    window.subscribed_to_event = false
                 }
                 if(("promotions").indexOf(type1) > -1 || ("promotions").indexOf(type2) > -1) {
+                    console.log("unsubscribing from promo",  window.subscribed_to_event );
                    window.subscribed_to_promo = false
                 } 
                 if(("stores").indexOf(type1) > -1 || ("stores").indexOf(type2) > -1) {
