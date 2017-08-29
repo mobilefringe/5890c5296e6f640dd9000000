a3d80e212c750708ce93c8da68de7fe6d66331be
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-08-29 11:42:03 -0400

    Twin Pine Demo 1.2  /site.js

--- site.js
+++ site.js
@@ -289,7 +289,7 @@ function errorSubscribedStatus () {
 
 function defaultSubscribedStatus () {
     console.log("default status");
-    if((("promotions").indexOf(type1) > -1 || ("promotions").indexOf(type2) > -1)) && (("events").indexOf(type1) > -1 || ("events").indexOf(type2) > -1)) {
+    if((("promotions").indexOf(type1) > -1 || ("promotions").indexOf(type2) > -1) && (("events").indexOf(type1) > -1 || ("events").indexOf(type2) > -1)) {
         
     }
     else {
