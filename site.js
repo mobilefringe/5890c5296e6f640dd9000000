f4fdedc081469b9c0c646908e64d2652e615e340
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-09-01 15:09:11 -0400

    Twin Pine Demo 1.2  /site.js

--- site.js
+++ site.js
@@ -55,7 +55,7 @@ if ('serviceWorker' in navigator && 'PushManager' in window) {
     console.log( 'Service Worker Error');
   });
 } else {
-    console.log( document.getElementsByClassName('popup_home'));
+    console.log( document.getElementsByClassName('popup_home').style.visibility="hidden";);
     $('#getStoreNotification').remove();
     $('.popup_home').hide();
     $('.receiveNotification').hide();
